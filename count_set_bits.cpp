2bb10e43255661c60bb227a05acec8736ae059d0
Vortex-21 <101874272+Vortex-21@users.noreply.github.com>
2022-05-23 21:18:29 +0530

    Add files via upload

---
+++ count_set_bits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int noofsetbits(int n)
+{
+    int x=0;
+    while(n>0)
+    {
+        n=n&(n-1);
+        x+=1;
+    }
+    return x;
+}
+int main()
+{
+
+    cout<<noofsetbits(6)<<endl;
+}
